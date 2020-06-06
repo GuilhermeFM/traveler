def58739b90728502474330464323a242dc96e38
Guilherme F Meira <guifrizzera2005@gmail.com>
2020-06-06 20:17:14 -0300

    Added permission checks for ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar, PermissionsAndroid } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const askPermission = async (): Promise<void> => {
+      await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+      await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION);
+    };
+
+    askPermission();
+  });
+
   return (
     <NavigationContainer>
       <StatusBar translucent backgroundColor="transparent" />
-      {/* <StatusBar hidden /> */}
       <Routes />
     </NavigationContainer>
   );
